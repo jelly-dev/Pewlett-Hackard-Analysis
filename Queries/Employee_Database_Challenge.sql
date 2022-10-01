1a21a83f77b3a6245c8fa693fd71e8201d1c6c49
AD <diazamarie@live.com>
2022-09-30 20:43:37 -0700

    challenge complete

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -34,3 +34,35 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+---------------------------------------------------------------
+
+-- Deliverable 2: Employees Eligible for the Mentorship Program
+
+-- Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+-- Retrieve the from_date and to_date columns from the Department Employee table.
+-- Retrieve the title column from the Titles table.
+-- Use a DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+-- Create a new table using the INTO clause.
+-- Join the Employees and the Department Employee tables on the primary key.
+-- Join the Employees and the Titles tables on the primary key.
+-- Filter the data on the to_date column to all the current employees, then filter the data on the birth_date columns to get all the employees whose birth dates are between January 1, 1965 and December 31, 1965.
+-- Order the table by the employee number.
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dep_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, ti.from_date DESC;
+----------------------------------------------------------------
